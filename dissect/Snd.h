345bc43350cf3972bd9609e7ee999d40a1feec70
Chris Sammis <csammis@gmail.com>
2015-07-09 08:02:09 -0500

    Prettier indention, started printing AIFF sample rate

--- dissect/Snd.h
+++ dissect/Snd.h
@@ -4,10 +4,24 @@
 #include "../NiceStructures.h"
 #include <stdbool.h>
 
+void Indent(int spaces)
+{
+    for (int i = 0; i < spaces; i++)
+    {
+        printf(" ");
+    }
+}
+
 void DissectCompressedSoundHeader(struct Resource* pResource, uint32_t offset)
 {
     uint32_t frameCount = OSReadBigInt32(pResource->data, offset);
-    // +10 for the AIFF sample rate 4
+    uint8_t aiffBuffer[10];
+    for (uint8_t i = 0; i < 10; i++)
+    {
+        aiffBuffer[9 - i] = *(pResource->data + offset + 4 + i);
+    }
+    long double aiffSampleRate = 0;
+    memcpy(&aiffSampleRate, aiffBuffer, 10); 
     // +4 for a marker chunk unused in 'snd ' 14
     char compressionFormat[5];
     memset(&compressionFormat, 0, 5);
@@ -20,8 +34,9 @@ void DissectCompressedSoundHeader(struct Resource* pResource, uint32_t offset)
     // +2 for snthID which is unused 38
     uint16_t sampleSize = OSReadBigInt16(pResource->data, offset + 40);
 
-    printf("*** Frame count: 0x%08x\n", frameCount);
-    printf("*** Compression format: ");
+    Indent(8); printf("Frame count: 0x%08x\n", frameCount);
+    Indent(8); printf("AIFF sample rate: %Lf\n", aiffSampleRate);
+    Indent(8); printf("Compression format: ");
     switch (compressionID)
     {
         case -2:
@@ -43,7 +58,7 @@ void DissectCompressedSoundHeader(struct Resource* pResource, uint32_t offset)
             printf("!!! unrecognized");
     }
     printf("\n");
-    printf("*** Original sample size: %d\n", sampleSize);
+    Indent(8); printf("Original sample size: %d\n", sampleSize);
 }
 
 void DissectSoundHeader(struct Resource* pResource, uint32_t offset)
@@ -56,23 +71,23 @@ void DissectSoundHeader(struct Resource* pResource, uint32_t offset)
     uint8_t encoding = (uint8_t)*(pResource->data + offset + 20);
     uint8_t baseFrequency = (uint8_t)*(pResource->data + offset + 21);
 
-    printf("*** Channels: %d\n", numChannels);
-    printf("*** Sample rate: 0x%08x\n", sampleRate);
+    Indent(8); printf("Channels: %d\n", numChannels);
+    Indent(8); printf("Sample rate: 0x%08x\n", sampleRate);
 
     switch (encoding)
     {
         case 0x00:
-            printf("*** Loop start: 0x%08x to loop end: 0x%08x\n", loopStart, loopEnd);
-            printf("*** Base frequency: 0x%02x\n", baseFrequency);
+            Indent(8); printf("*** Loop start: 0x%08x to loop end: 0x%08x\n", loopStart, loopEnd);
+            Indent(8); printf("*** Base frequency: 0x%02x\n", baseFrequency);
             break;
         case 0xFE:
             DissectCompressedSoundHeader(pResource, offset + 22);
             break;
         case 0xFF:
-            printf("*** !!! Extended sound header, not sure what this is\n");
+            Indent(8); printf("!!! Extended sound header, not sure what this is\n");
             break;
         default:
-            printf("*** !!! Unrecognized sound header encoding: 0x%02x\n", encoding);
+            Indent(8); printf("!! Unrecognized sound header encoding: 0x%02x\n", encoding);
     }
 }
 
@@ -84,7 +99,7 @@ void DissectSingleSoundCommand(struct Resource* pResource, uint16_t rawCommand, 
     switch (command)
     {
         case 0x51:
-            printf("** bufferCmd");
+            Indent(6); printf("bufferCmd");
             if (hasAssociatedSoundData == false)
             {
                 printf(" with sound header located outside resource at 0x%08x\n", param2);
@@ -96,28 +111,28 @@ void DissectSingleSoundCommand(struct Resource* pResource, uint16_t rawCommand, 
             }
             break;
         default:
-            printf("** !!! Unrecognized sound command: 0x%04x (param1: 0x%04x param2: 0x%08x)\n", command, param1, param2);
+            Indent(6); printf("!!! Unrecognized sound command: 0x%04x (param1: 0x%04x param2: 0x%08x)\n", command, param1, param2);
     }
 }
 
 void DissectSingleSoundResource(struct Resource* pResource)
 {
     printf("Dissecting named resource '%s'\n", pResource->name);
-    printf("Sound Resource Header\n");
-    printf("* Format Type: 0x%04x\n", OSReadBigInt16(pResource->data, 0));
+    Indent(2); printf("Sound Resource Header\n");
+    Indent(4); printf("Format Type: 0x%04x\n", OSReadBigInt16(pResource->data, 0));
     
     uint16_t dataTypeCount = OSReadBigInt16(pResource->data, 2);
-    printf("* Number of data types: 0x%04x\n", dataTypeCount);
+    Indent(4); printf("Number of data types: 0x%04x\n", dataTypeCount);
 
     uint16_t offset = 4;
     for (uint16_t i = 0; i < dataTypeCount; i++)
     {
-        printf("** Data type: 0x%04x\n", OSReadBigInt16(pResource->data, offset));
-        printf("** Initialization options: 0x%08x\n", OSReadBigInt32(pResource->data, offset + 2));
+        Indent(6); printf("Data type: 0x%04x\n", OSReadBigInt16(pResource->data, offset));
+        Indent(6); printf("Initialization options: 0x%08x\n", OSReadBigInt32(pResource->data, offset + 2));
         offset += 6;
     }
 
-    printf("Sound Commands\n");
+    Indent(2); printf("Sound Commands\n");
     uint16_t commandCount = OSReadBigInt16(pResource->data, offset);
     offset += 2;
     for (uint16_t i = 0; i < commandCount; i++)
@@ -126,7 +141,7 @@ void DissectSingleSoundResource(struct Resource* pResource)
         uint16_t param1 = OSReadBigInt16(pResource->data, offset + 2);
         uint32_t param2 = OSReadBigInt32(pResource->data, offset + 4);
 
-        printf("* Command %d:\n", i + 1);
+        Indent(4); printf("Command %d:\n", i + 1);
         DissectSingleSoundCommand(pResource, command, param1, param2);
         offset += 8;
     }
