729ac36e88d755d450296ff0fd507f782e6377dc
Chris Sammis <csammis@gmail.com>
2015-07-24 16:20:58 -0500

    Expansion of pattern-initialized data sections

--- pef/PEF.h
+++ pef/PEF.h
@@ -30,6 +30,7 @@ struct SectionData
     uint8_t type;
     uint8_t* data;
     uint32_t length;
+    uint32_t totalLength;
 };
 
 void ProcessLoaderSection(uint8_t* loaderSection, struct LoaderSection* pSection)
@@ -181,6 +182,125 @@ void ProcessCodeSection(struct SectionData* pSection, struct LoaderSection* pLoa
     free(labels);
 }
 
+#define PATTERN_DATA_OPCODE(x) ((x & 0xE0) >> 5)
+
+uint32_t ReadOnePatternArgument(uint8_t* pData, bool firstArgument, uint32_t index, uint32_t* pNewIndex)
+{
+    uint32_t arg = 0;
+    if (firstArgument)
+    {
+        arg = pData[index++] & 0x1F;
+        if (arg == 0)
+        {
+            return ReadOnePatternArgument(pData, false, index, pNewIndex);
+        }
+    }
+    else
+    {
+        uint8_t piece = 0;
+        do
+        {
+            piece = pData[index++];
+            arg <<= 7;
+            arg |= (piece & 0x7F);
+        } while (piece & 0x80);
+    }
+
+    *pNewIndex = index;
+    return arg;
+}
+
+void ProcessPatternDataSection(struct SectionData* pSection)
+{
+    uint32_t dataLocationCount = 0;
+
+    uint8_t* unpackedData = malloc(pSection->totalLength);
+    memset(unpackedData, 0, pSection->totalLength);
+
+    for (uint32_t i = 0, instCount = 0; i < pSection->length; instCount++)
+    {
+        uint8_t opcode = PATTERN_DATA_OPCODE(pSection->data[i]);
+        uint32_t count = ReadOnePatternArgument(pSection->data, true, i, &i);
+
+        switch (opcode)
+        {
+            case 0: // Zero
+                memset(unpackedData + dataLocationCount, 0, count);
+                dataLocationCount += count;
+                break;
+            case 1: // blockCopy
+                memcpy(unpackedData + dataLocationCount, pSection->data + i, count);
+                dataLocationCount += count;
+                i += count;
+                break;
+            case 2: // repeatedBlock
+                {
+                    uint32_t repeatCount = ReadOnePatternArgument(pSection->data, false, i, &i);
+                    for (uint32_t j = 0; j < repeatCount; j++)
+                    {
+                        memcpy(unpackedData + dataLocationCount, pSection->data + i, count);
+                        dataLocationCount += count;
+                    }
+                    i += (repeatCount * count);
+                }
+                break;
+            case 3:
+                {
+                    uint32_t customSize = ReadOnePatternArgument(pSection->data, false, i, &i);
+                    uint32_t repeatCount = ReadOnePatternArgument(pSection->data, false, i, &i);
+
+                    for (uint32_t j = 0; j < repeatCount; j++)
+                    {
+                        // Common data
+                        memcpy(unpackedData + dataLocationCount, pSection->data + i, count);
+                        dataLocationCount += count;
+                        // Custom data j
+                        memcpy(unpackedData + dataLocationCount, pSection->data + i + count + (j * customSize), customSize);
+                        dataLocationCount += customSize;
+                    }
+
+                    memcpy(unpackedData + dataLocationCount, pSection->data + i, count);
+                    dataLocationCount += count;
+                    i += (count + (customSize * repeatCount));
+                }
+                break;
+            case 4:
+                {
+                    uint32_t customSize = ReadOnePatternArgument(pSection->data, false, i, &i);
+                    uint32_t repeatCount = ReadOnePatternArgument(pSection->data, false, i, &i);
+
+                    for (uint32_t j = 0; j < repeatCount; j++)
+                    {
+                        // Zeros instead of common data
+                        memset(unpackedData + dataLocationCount, 0, count);
+                        dataLocationCount += count;
+                        // Custom data j
+                        memcpy(unpackedData + dataLocationCount, pSection->data + i + count + (j * customSize), customSize);
+                        dataLocationCount += customSize;
+                    }
+
+                    memset(unpackedData + dataLocationCount, 0, count);
+                    dataLocationCount += count;
+                    i += (customSize * repeatCount);
+                }
+                break;
+            default:
+                printf("DEBUG: Unexpected opcode in packed data at %x: %d, count = %d", i, opcode, count);
+        }
+    }
+
+    printf("\nPattern-initialized data section %d:\n", pSection->id);
+    for (uint32_t i = 0; i < pSection->totalLength; i += 16)
+    {
+        printf("%16x:\t[ ", i);
+        for (uint32_t j = 0; j < 16; j++)
+        {
+            printf("%02x ", unpackedData[i + j]);
+        }
+        printf("]\n");
+    }
+}
+
 void ReadPEFSection(uint16_t sectionIndex, FILE* input, struct LoaderSection* pLoaderSection, struct SectionData* pSection)
 {
     uint16_t sectionOffset = 40 + (sectionIndex * 28);
@@ -240,6 +360,7 @@ void ReadPEFSection(uint16_t sectionIndex, FILE* input, struct LoaderSection* pL
         default:
             pSection->data = malloc(packedSize);
             pSection->length = packedSize;
+            pSection->totalLength = totalSize;
             fseek(input, containerOffset, SEEK_SET);
             fread(pSection->data, packedSize, 1, input);
     }
@@ -283,6 +404,9 @@ void ProcessPEF(FILE* input)
         {
             case 0:
                 ProcessCodeSection(sections[i], &loader);
+                break;
+            case 2:
+                ProcessPatternDataSection(sections[i]);
                 break;
             case 4:
                 // Loader - already handled
